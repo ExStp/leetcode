553b14a5f8951898d497ffa9407b4236ccd22e07
Andrey <48987161+ExStp@users.noreply.github.com>
2023-09-18 17:33:54 +0500

    new task

---
+++ Easy/2665_CounterII.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+	let count = init;
+
+	increment = function () {
+		return ++count;
+	};
+
+	decrement = function () {
+		return --count;
+	};
+	reset = function () {
+		return (count = init);
+	};
+	return { increment, decrement, reset };
+};
+
+let counter = createCounter(5);
+console.log(counter.increment());
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
